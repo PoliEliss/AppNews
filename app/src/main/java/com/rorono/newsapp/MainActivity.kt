eac443241916cb0b45d3cf264fc85bec7851f0fe
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-06-21 14:27:44 +0300

    create Navigation

--- app/src/main/java/com/rorono/newsapp/MainActivity.kt
+++ app/src/main/java/com/rorono/newsapp/MainActivity.kt
@@ -2,10 +2,28 @@ package com.rorono.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rorono.newsapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigationView.setupWithNavController(navController=navController)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/rorono/newsapp/screens/main/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.rorono.newsapp.screens.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rorono.newsapp.R
+import com.rorono.newsapp.databinding.FragmentDetailsBinding
+import com.rorono.newsapp.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
