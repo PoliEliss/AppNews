d1512f5a30fe25608c322dea4543903f7accdefe
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-06-27 10:28:53 +0300

    create ViewModel

--- app/src/main/java/com/rorono/newsapp/MainActivity.kt
+++ app/src/main/java/com/rorono/newsapp/MainActivity.kt
@@ -2,22 +2,29 @@ package com.rorono.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rorono.newsapp.data.api.RetrofitInstance
+import com.rorono.newsapp.data.testRepo.TestRepo
 import com.rorono.newsapp.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewModel: MainViewModel
     private var _binding: ActivityMainBinding? = null
     private val mBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
         val navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
-        bottomNavigationView.setupWithNavController(navController=navController)
+        bottomNavigationView.setupWithNavController(navController = navController)
+        val repository = TestRepo(retrofit = RetrofitInstance)
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
 
 
     }
