02e75962d77261b7bc2efb031ffb745611dbca31
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-06-27 10:25:47 +0300

    create Adapter

---
+++ app/src/main/java/com/rorono/newsapp/AdapterNews.kt
@@ -0,0 +1,23 @@
+package com.rorono.newsapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterNews:RecyclerView.Adapter<AdapterNews.NewsViewHolder>() {
+   inner class NewsViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
